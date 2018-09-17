714e5c9e23483e808d1170e780a23d4218bfc351
Denis Viklov <denis.viklov@gmail.com>
2018-09-18 05:05:09 +0800

    turn into the package

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+import url_health
+
+
+setup(
+    name='django-url-health',
+    version=url_health.__version__,
+    description='Check URLs',
+    long_description=open('README.rst').read(),
+    author='Denis Viklov',
+    author_email='denis.viklov@gmail.com',
+    url='https://github.com/denisviklov/django-url-health',
+    license='Apache v2.0',
+    packages=find_packages(exclude=['test_settings.py', 'mockapp']),
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
